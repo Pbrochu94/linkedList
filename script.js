1b5abe1d416c117c73aa8ba5650aee3113a7031e
Patrick Brochu <patrickbrochu70@gmail.com>
2025-05-29 11:24:05 -0400

    - Initialized files - Created class for list and node

---
+++ script.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  append = (node) => {};
+  prepend = (node) => {};
+  size = () => {};
+  head = () => {};
+  tail = () => {};
+  at = (index) => {};
+  pop = () => {};
+  contains = (value) => {};
+  find = (value) => {};
+  toString = () => {};
+}
